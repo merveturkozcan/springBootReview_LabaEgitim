af1713aba1000e255aa705f6e7866e6ec4d0e72d
merve türk <mtturk26@gmail.com>
2024-03-01 22:30:20 +0100

    jdbc Project third commit

---
+++ src/test/java/com/merveturk/springClassLaba/SpringClassLabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merveturk.springClassLaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClassLabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
